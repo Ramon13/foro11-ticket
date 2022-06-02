be36375f7a1bb46068ede81ce420930bdf9bb9a6
Ramon Santos da Costa <ramoncosta@redejmu.local>
2022-06-02 18:40:04 -0300

    created register form and validate title and description fiels

--- src/app/tickets/ticket-register/ticket-register.component.ts
+++ src/app/tickets/ticket-register/ticket-register.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { AngularEditorConfig } from '@kolkov/angular-editor';
+import { FormBuilder, Validators } from '@angular/forms';
+import { PRIORITIES, AUDITORSHIPS } from '../../tickets';
 
 @Component({
   selector: 'app-ticket-register',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ticket-register.component.css']
 })
 export class TicketRegisterComponent implements OnInit {
+  
+  ticketForm = this.fb.group({
+    title: ["", [
+      Validators.minLength(4),
+      Validators.maxLength(32),
+      Validators.required
+    ]],
+    description: ["", [
+      Validators.minLength(4),
+      Validators.required
+    ]]
+  });
 
-  constructor() { }
+  editorConfig: AngularEditorConfig = {
+    editable: true,
+    minHeight: '100'
+  }
+  
+  priorityList = PRIORITIES;
+  auditorshipList = AUDITORSHIPS;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
   }
-
 }
